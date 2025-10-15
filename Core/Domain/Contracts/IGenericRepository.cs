583924b0d3bca4b30001f8500596162356de6e85
mohamed shawky <shawky1mohamed2@gmail.com>
2025-10-15 19:46:25 +0300

    GenricRepo

---
+++ Core/Domain/Contracts/IGenericRepository.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Entites.ProductModule;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Contracts
+{
+    public interface IGenericRepository<TEntity,TKey>where TEntity : BaseEntity<TKey>
+    {
+        Task<IEnumerable<TEntity>> GetAllAsync(bool asNoTracking=false);
+
+        Task<TEntity?>GetByIdAsync(TKey id);
+
+        Task AddAsync(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+
+    }
+}
