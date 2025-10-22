6e72ff3c3a07f3d800940b1ca6272f9fce5ab9f1
mohamed shawky <shawky1mohamed2@gmail.com>
2025-10-22 20:09:37 +0300

    Introduce specification pattern in repository layer
    Added `ISpecifications` interface and `BaseSpecifications` class to define reusable query logic. Updated `IGenericRepository` and `GenericRepository` to support querying entities using specifications. Implemented `SpecificationEvaluator` to build queries based on specifications.
    
    Updated `Domain.csproj` to exclude `Exceptions` and `Global` folders from the build process. Added placeholder `Exceptions` and `Global` classes under the `Domain` namespace.

---
+++ Core/Domain/Contracts/ISpecifications.cs
@@ -0,0 +1,15 @@
+﻿
+
+using Domain.Entites.ProductModule;
+using System.Linq.Expressions;
+
+namespace Domain.Contracts
+{
+    public interface ISpecifications<TEntity,TKey>where TEntity : BaseEntity<TKey>
+    {
+        public Expression<Func<TEntity,bool>> Criteria { get; }
+        public List<Expression<Func<TEntity,Object>>> IncludeExpressions { get; }
+
+
+    }
+}
---
+++ Infrastructure/Presistence/SpecificationEvaluator.cs
@@ -0,0 +1,32 @@
+﻿using Domain.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presistence
+{
+    internal static class SpecificationEvaluator
+    {
+        public static IQueryable<TEntity> CreateQuery<TEntity,TKey>(
+            IQueryable<TEntity>InputQuery, ISpecifications<TEntity, TKey> specifications
+            ) where TEntity :BaseEntity<TKey>
+        {
+            var query = InputQuery;
+
+            if(specifications.Criteria is not null)
+            {
+                query=query.Where(specifications.Criteria);
+            }
+            if(specifications.IncludeExpressions is not null 
+                && specifications.IncludeExpressions.Count > 0)
+            {
+                query = specifications.IncludeExpressions
+                    .Aggregate(query, (cur, ex) => cur.Include(ex));
+            }
+            return query;
+
+        }
+    }
+}
