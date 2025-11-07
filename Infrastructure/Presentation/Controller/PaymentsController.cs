5f39cbb4e299c07af5f81701b475e707778de4b0
mohamed shawky <shawky1mohamed2@gmail.com>
2025-11-07 22:19:19 +0200

    Add controller

---
+++ Infrastructure/Presentation/Controller/PaymentsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.Abstraction;
+using Shared.DTos.BasketModule;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation.Controller
+{
+    public class PaymentsController(IServiceManger serviceManger) : ApiController
+    {
+        [HttpPost("{Basketid}")]
+        public async Task<ActionResult<BasketDto>> CreateOrUpdatePaymentIntent(string Basketid)
+        =>Ok( await serviceManger.PaymentService.CreateOrUpdatePaymentIntentAsync(Basketid));
+            
+        
+    }
+}
